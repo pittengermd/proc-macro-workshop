67cbf4620be98a682c6fdf25389da5adde1ada75
Matt Pittenger <pittengermdp@gmail.com>
2022-12-01 09:25:21 -0600

    Test 2 passes.

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,4 +1,4 @@
-use proc_macro2::TokenStream;
+use proc_macro2::{TokenStream, Ident};
 use quote::{quote, quote_spanned};
 use syn::spanned::Spanned;
 use syn::{
@@ -9,9 +9,23 @@ use syn::{
 pub fn derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     let input = parse_macro_input!(input as DeriveInput);
     let name = input.ident;
+    let builder_name = Ident::new(&format!("{}Builder", name), name.span());
     quote! {
-        impl Builder for #name {
-            fn builder() {}
+        pub struct #builder_name {
+            executable: Option<String>,
+            args: Option<Vec<String>>,
+            env: Option<Vec<String>>,
+            current_dir: Option<String>,
+        }
+        impl #name {
+            fn builder() -> #builder_name {
+                #builder_name {
+                    executable: Default::default(),
+                    args: Default::default(),
+                    env: Default::default(),
+                    current_dir: Default::default(),
+                }
+            }
         }
     }.into()
 }
