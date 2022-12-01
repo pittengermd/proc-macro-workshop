4cda7e3bc56b74336f674e0254b767b1daf14386
Matt Pittenger <pittengermdp@gmail.com>
2022-12-01 09:04:40 -0600

    Initial Commit

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,8 +1,17 @@
-use proc_macro::TokenStream;
+use proc_macro2::TokenStream;
+use quote::{quote, quote_spanned};
+use syn::spanned::Spanned;
+use syn::{
+    parse_macro_input, parse_quote, Data, DeriveInput, Fields, GenericParam, Generics, Index,
+};
 
 #[proc_macro_derive(Builder)]
-pub fn derive(input: TokenStream) -> TokenStream {
-    let _ = input;
-
-    unimplemented!()
+pub fn derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
+    let input = parse_macro_input!(input as DeriveInput);
+    let name = input.ident;
+    quote! {
+        impl Builder for #name {
+            fn builder() {}
+        }
+    }.into()
 }
--- debug/src/lib.rs
+++ debug/src/lib.rs
@@ -1,8 +1,48 @@
-use proc_macro::TokenStream;
+use proc_macro2::TokenStream;
+use quote::{quote, quote_spanned};
+use syn::spanned::Spanned;
+use syn::{
+    parse_macro_input, parse_quote, Data, DeriveInput, Fields, GenericParam, Generics, Index,
+};
+
 
 #[proc_macro_derive(CustomDebug)]
-pub fn derive(input: TokenStream) -> TokenStream {
-    let _ = input;
+pub fn derive(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
+    let input = syn::parse_macro_input!(input as syn::DeriveInput);
+    let name = input.ident;
+    let fields = parse_fields(&input.data);
+    let expanded = quote::quote! {
+        impl std::fmt::Debug for #name {
+            fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+                f.debug_struct(#name)
+                #fields
+                .finish()
+            }
+        }
+    };
+    proc_macro::TokenStream::from(expanded)
+}
 
-    unimplemented!()
+fn parse_fields(data: &syn::Data) -> TokenStream {
+    match *data {
+        Data::Struct(ref data) => {
+            match data.fields {
+                Fields::Named(ref fields) => {
+                    fields.named.iter().map(|f| {
+                        let name = &f.ident;
+                        quote! {
+                            .field(#name, &self.#name)
+                        }
+                    }).collect()
+                },
+                Fields::Unnamed(ref _fields) => {
+                    quote! {}
+                },
+                Fields::Unit => {
+                    quote!{}
+                },
+            }
+        },
+        syn::Data::Enum(_) | syn::Data::Union(_) => unimplemented!(),
+    }
 }
